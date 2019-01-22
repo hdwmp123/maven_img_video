1d501ea34de009e02d2cba2ab5b1488250303dd0
wangmipeng <wangmipeng@fangshiliu.com>
2019-01-22 18:04:19 +0800

    1 功能完善

--- src/main/java/com/king/img/ImageUtils.java
+++ src/main/java/com/king/img/ImageUtils.java
@@ -8,6 +8,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Iterator;
 
 import javax.imageio.ImageIO;
@@ -99,15 +101,46 @@ public class ImageUtils {
     }
 
     public static void main(String[] args) throws Exception {
-        String src = "D:/Img/in/001.png";
-        int[] imgWidthHeight = ReduceImg.getImgWidthHeight(new File(src));
-        int width = imgWidthHeight[0];
-        int height = imgWidthHeight[1];
-        int index = 1;
-        for (int i = 0; i < height; i += width * 1) {
-            ImageUtils.cutPNG(new FileInputStream(src), new FileOutputStream(String.format("D:/Img/out/%s.png", index)), 0,
-                    i, width, width * 1);
-            index++;
+//        String src = "D:/Img/in/001.png";
+//        int[] imgWidthHeight = ReduceImg.getImgWidthHeight(new File(src));
+//        int width = imgWidthHeight[0];
+//        int height = imgWidthHeight[1];
+//        int index = 1;
+//        for (int i = 0; i < height; i += width * 1) {
+//            ImageUtils.cutPNG(new FileInputStream(src), new FileOutputStream(String.format("D:/Img/out/%s.png", index)), 0,
+//                    i, width, width * 1);
+//            index++;
+//        }
+        String src = "C:\\Users\\chanp\\Desktop\\Img\\小程序\\2.jpg";
+        File file = new File(src);
+        int[] imgWidthHeight = ReduceImg.getImgWidthHeight(file);
+        double w_h_scale = divide(1125,2436);//标准宽高比
+        int relWidth = imgWidthHeight[0];
+        int relHeight = imgWidthHeight[1];
+        double real_scale = divide(relWidth,relHeight);//实际宽高比
+        String absolutePath = file.getParentFile().getAbsolutePath();
+        String out = String.format("%s/out/%s", absolutePath,file.getName());
+        if (real_scale > w_h_scale) {//胖
+            //高度为标准，宽度裁剪
+            int width = (int) (relHeight * w_h_scale);
+            int height = relHeight;
+            int x = (relWidth-width) / 2;
+            int y = 0;
+            System.out.println(String.format("x:%s,y:%s,width:%s,height:%s", x,y,width,height));
+            ImageUtils.cutJPG(new FileInputStream(src), new FileOutputStream(out), x, y, width, height);
+        } else { // 瘦
+            // 宽度为标准，高度裁剪
+            int width = relWidth;
+            int height = (int) (relWidth / w_h_scale);
+            int x = 0;
+            int y = (relHeight-height) / 2;
+            System.out.println(String.format("x:%s,y:%s,width:%s,height:%s", x,y,width,height));
+            ImageUtils.cutJPG(new FileInputStream(src), new FileOutputStream(out), x, y, width, height);
         }
+        System.out.println("OK");
+    }
+    
+    private static double divide(int a,int b) {
+        return new BigDecimal(a).divide(new BigDecimal(b),10,RoundingMode.UP).doubleValue();
     }
 }
---
+++ src/main/java/com/king/png/PngExe.java
@@ -0,0 +1,73 @@
+package com.king.png;
+
+import java.awt.Color;
+import java.awt.image.*;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.io.File;
+import java.io.FileInputStream;
+import javax.imageio.ImageIO;
+/**
+ * 
+ * ClassName: PngExe <br/> 
+ * Reason: png特定颜色转换. <br/> 
+ * date: 2019年1月22日 下午3:37:29 <br/> 
+ * 
+ * @author KingTiger 
+ * @version  
+ * @since JDK 1.8
+ * @see https://ask.csdn.net/questions/674484?sort=id
+ */
+public class PngExe extends RGBImageFilter {
+    public static void main(String[] args) {
+        File dir = new File("D:\\Img\\cond-icon-heweather");
+        File[] fileList = dir.listFiles();
+        for (File file : fileList) {
+            String fileName = file.getName();
+            System.out.println(String.format("处理图片:%s",fileName));
+            if(!fileName.toLowerCase().contains(".png")) {
+                continue;
+            }
+            try {
+                BufferedImage imageBiao = ImageIO.read(new FileInputStream(file));
+                ImageFilter imgf = new PngExe(new Color(71,89,120).getRGB(),Color.WHITE.getRGB()); //白色
+                FilteredImageSource fis = new FilteredImageSource(imageBiao.getSource(), imgf);
+                Image im = Toolkit.getDefaultToolkit().createImage(fis);
+                im.flush();
+                BufferedImage newImage = new BufferedImage(imageBiao.getWidth(), imageBiao.getHeight(), BufferedImage.TYPE_INT_ARGB);
+                Graphics2D g = newImage.createGraphics();
+                g.drawImage(im, 0, 0, null);
+                g.dispose();
+                newImage.flush();
+                ImageIO.write(newImage, "png", new File(String.format("D:\\Img\\new_png\\%s", fileName)));
+                // 把以上原图和加上图标后的图像
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("OK");
+    }
+    // 继承它实现图象ARGB的处理
+    int sourceRgb = 0;
+    int targetRgb = 0;
+
+    public PngExe(int sourceRgb,int targetRgb) {// 构造器，用来接收需要过滤图象的尺寸，以及透明度
+        this.canFilterIndexColorModel = true;
+        this.sourceRgb = sourceRgb;
+        this.targetRgb = targetRgb;
+    }
+    DirectColorModel dcm = (DirectColorModel) ColorModel.getRGBdefault();
+
+    public int filterRGB(int x, int y, int rgb) {
+        int alp = dcm.getAlpha(rgb);
+        if (alp != 0) {
+            return targetRgb;
+        }
+        if (dcm.getRGB(rgb) == dcm.getRGB(this.sourceRgb)) {
+            System.out.println("same");
+            return targetRgb;
+        }
+        return alp << 24 | dcm.getRGB(rgb);// 进行标准ARGB输出以实现图象过滤
+    }
+}
